a7c71c683f7cd74d6c46ce88290128f4b9cb50e0
rd6668 <rendi6668@gmail.com>
2014-04-26 15:43:00 -0400

    edit connectToDB and GetDBinfo

--- CS562/src/connectToDB/Get_DB_info.java
+++ CS562/src/connectToDB/Get_DB_info.java
@@ -1,9 +1,14 @@
 package connectToDB;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class Get_DB_info {
 	public static ArrayList<String> get_db_info_schema(){
+		ResultSet rs_type;      //resultset object gets the set of values retreived from the database
+		String query_type = "SELECT data_type,column_name from information_schema.\"columns\" WHERE \"table_name\"='sales' ";
+		rs_type = st.executeQuery(query_type);              //executing the query 
+		
 		
 		
 		return null;
