00f7d5519d5dc1fae2224fb10561770f0a1982d6
jgaoxxxx <gao.jian0630@gmail.com>
2014-05-09 16:30:59 -0400

    change code

--- CS562/src/Generator.java
+++ CS562/src/Generator.java
@@ -131,25 +131,43 @@ public class Generator{
                     output.print("\t\t\tnewtuple."+entry.getKey()+" = rs.getInt(\""+entry.getKey()+"\");\n");
                 }
             }
+            boolean flag = false;
+            output.print("\t\t\tif(");
+            if (qs.where.size() == 0){
+                output.print("true");
+            }
+            else{
+                for (String temp : qs.where){
+                    if (flag == false){
+                        output.print(temp);
+                        flag = true;
+                    }
+                    else if (flag == true)
+                        output.print(" && "+temp);
+                }
+            }
+            flag = false;
+            output.print("){\n");
             for (Info info : qs.aggregate_function_info){
                 if (info.aggregate.equals("sum")){
-                    output.print("\t\t\t"+info.getName()+" += newtuple."+info.attribute+";\n");
+                    output.print("\t\t\t\t"+info.getName()+" += newtuple."+info.attribute+";\n");
                 }
                 else if (info.aggregate.equals("max")){
-                    output.print("\t\t\t"+info.getName()+" = ("+info.getName()+"< newtuple."+info.attribute+") ? newtuple."+info.attribute +" : "+info.getName()+";\n");
+                    output.print("\t\t\t\t"+info.getName()+" = ("+info.getName()+"< newtuple."+info.attribute+") ? newtuple."+info.attribute +" : "+info.getName()+";\n");
                 }
                 else if (info.aggregate.equals("min")){
-                    output.print("\t\t\t"+info.getName()+" = ("+info.getName()+"> newtuple."+info.attribute+") ? newtuple."+info.attribute +" : "+info.getName()+";\n");
+                    output.print("\t\t\t\t"+info.getName()+" = ("+info.getName()+"> newtuple."+info.attribute+") ? newtuple."+info.attribute +" : "+info.getName()+";\n");
                 }
                 else if (info.aggregate.equals("count")){
-                    output.print("\t\t\t"+info.getName()+" ++;\n");
+                    output.print("\t\t\t\t"+info.getName()+" ++;\n");
                 }
                 else if (info.aggregate.equals("avg")){
-                    output.print("\t\t\tsum_"+info.attribute+"_"+info.index+" += newtuple."+info.attribute+";\n");
-                    output.print("\t\t\tcount_"+info.attribute+"_"+info.index+" ++;\n");
+                    output.print("\t\t\t\tsum_"+info.attribute+"_"+info.index+" += newtuple."+info.attribute+";\n");
+                    output.print("\t\t\t\tcount_"+info.attribute+"_"+info.index+" ++;\n");
 
                 }
             }
+            output.print("\t\t\t}\n");
             output.print("\t\t\tmore=rs.next();\n");
             output.print("\t\t}\n\n");
         }
@@ -171,8 +189,24 @@ public class Generator{
             }
 
             //output if(condition)
-            output.print("\t\t\tif (");
             boolean flag = false;
+            output.print("\t\t\tif(");
+            if (qs.where.size() == 0){
+                output.print("true");
+            }
+            else{
+                for (String temp : qs.where){
+                    if (flag == false){
+                        output.print(temp);
+                        flag = true;
+                    }
+                    else if (flag == true)
+                        output.print(" && "+temp);
+                }
+            }
+            flag = false;
+            output.print("){\n");
+            output.print("\t\t\t\tif (");
             for (Condition temp : qs.condition){
                 if (Integer.parseInt(temp.index) == i && flag == false){
                     flag = true;
@@ -185,12 +219,12 @@ public class Generator{
             if (flag == false)
                     output.print("true");
             flag = false;
-            output.print("){\t\n");
-            output.print("\t\t\t\tboolean found = false;\n");
-            output.print("\t\t\t\tfor (MF_structure temp : result_list){\n");
+            output.print("){\n");
+            output.print("\t\t\t\t\tboolean found = false;\n");
+            output.print("\t\t\t\t\tfor (MF_structure temp : result_list){\n");
 
             //find if the grouping attributes are already in the list
-            output.print("\t\t\t\t\tif(compare(temp.");
+            output.print("\t\t\t\t\t\tif(compare(temp.");
             for (String temp : qs.grouping_attributes){
                 if (flag == false){
                     output.print(temp+",newtuple."+temp+")");
@@ -200,57 +234,58 @@ public class Generator{
                     output.print(" && compare(temp."+temp+",newtuple."+temp+")");
                 }
             }
-            output.print("){\t\n");
+            output.print("){\n");
             for (Info info : qs.select_attr_info){
                 if (Integer.parseInt(info.index) == i){
                     if (info.aggregate.equals("avg")){
-                        output.print("\t\t\t\t\t\ttemp.sum_"+info.attribute+"_"+info.index+" += newtuple."+info.attribute+";\n");
-                        output.print("\t\t\t\t\t\ttemp.count_"+info.attribute+"_"+info.index+" ++;\n");
+                        output.print("\t\t\t\t\t\t\ttemp.sum_"+info.attribute+"_"+info.index+" += newtuple."+info.attribute+";\n");
+                        output.print("\t\t\t\t\t\t\ttemp.count_"+info.attribute+"_"+info.index+" ++;\n");
                     }
                     if (info.aggregate.equals("sum")){
-                        output.print("\t\t\t\t\t\ttemp."+info.getName()+" += newtuple."+info.attribute+";\n");
+                        output.print("\t\t\t\t\t\t\ttemp."+info.getName()+" += newtuple."+info.attribute+";\n");
                     }
                     if (info.aggregate.equals("max")){
-                        output.print("\t\t\t\t\t\ttemp."+info.getName()+" = (temp."+info.getName()+"< newtuple."+info.attribute+") ? newtuple."+info.attribute +" :temp."+info.getName()+";\n");
+                        output.print("\t\t\t\t\t\t\ttemp."+info.getName()+" = (temp."+info.getName()+"< newtuple."+info.attribute+") ? newtuple."+info.attribute +" :temp."+info.getName()+";\n");
                     }
                     if (info.aggregate.equals("min")){
-                        output.print("\t\t\t\t\t\ttemp."+info.getName()+" = (temp."+info.getName()+"> newtuple."+info.attribute+") ? newtuple."+info.attribute +" :temp."+info.getName()+";\n");
+                        output.print("\t\t\t\t\t\t\ttemp."+info.getName()+" = (temp."+info.getName()+"> newtuple."+info.attribute+") ? newtuple."+info.attribute +" :temp."+info.getName()+";\n");
                     }
                     if (info.aggregate.equals("count")){
-                        output.print("\t\t\t\t\t\ttemp."+info.getName()+" ++;\n");
+                        output.print("\t\t\t\t\t\t\ttemp."+info.getName()+" ++;\n");
                     }
                 }
             }
             //output.print("\t\t\t\t\ttemp.\n");
-            output.print("\t\t\t\t\t\tfound=true;\n");
+            output.print("\t\t\t\t\t\t\tfound=true;\n");
+            output.print("\t\t\t\t\t\t}\n");
             output.print("\t\t\t\t\t}\n");
-            output.print("\t\t\t\t}\n");
-            output.print("\t\t\t\tif (found == false){\n");
-            output.print("\t\t\t\t\tMF_structure newrow = new MF_structure();\n");
+            output.print("\t\t\t\t\tif (found == false){\n");
+            output.print("\t\t\t\t\t\tMF_structure newrow = new MF_structure();\n");
             for (String temp : qs.grouping_attributes){
-                output.print("\t\t\t\t\tnewrow."+temp+" = newtuple."+temp+";\n");
+                output.print("\t\t\t\t\t\tnewrow."+temp+" = newtuple."+temp+";\n");
             }
             for (Info temp : qs.select_attr_info){
                 if (temp.aggregate.equals("avg") && Integer.parseInt(temp.index) == i){
-                    output.print("\t\t\t\t\tnewrow.sum_"+temp.attribute+"_"+temp.index+" = newtuple."+temp.attribute+";\n");
-                    output.print("\t\t\t\t\tnewrow.count_"+temp.attribute+"_"+temp.index+" = 1;\n");
+                    output.print("\t\t\t\t\t\tnewrow.sum_"+temp.attribute+"_"+temp.index+" = newtuple."+temp.attribute+";\n");
+                    output.print("\t\t\t\t\t\tnewrow.count_"+temp.attribute+"_"+temp.index+" = 1;\n");
                 }
                 if ((temp.aggregate.equals("sum") || temp.aggregate.equals("max") || temp.aggregate.equals("min")) && Integer.parseInt(temp.index) == i)
-                    output.print("\t\t\t\t\tnewrow."+temp.getName()+" = newtuple."+temp.attribute+";\n");
+                    output.print("\t\t\t\t\t\tnewrow."+temp.getName()+" = newtuple."+temp.attribute+";\n");
                 if (temp.aggregate.equals("count") && Integer.parseInt(temp.index) == i)
-                    output.print("\t\t\t\t\tnewrow."+temp.getName()+" = 1;\n");
+                    output.print("\t\t\t\t\t\tnewrow."+temp.getName()+" = 1;\n");
                 if (Integer.parseInt(temp.index) != i){
                     if(temp.aggregate.equals("avg")){
-                        output.print("\t\t\t\t\tnewrow.sum_"+temp.attribute+"_"+temp.index+" = 0;\n");
-                        output.print("\t\t\t\t\tnewrow.count_"+temp.attribute+"_"+temp.index+" = 0;\n");
+                        output.print("\t\t\t\t\t\tnewrow.sum_"+temp.attribute+"_"+temp.index+" = 0;\n");
+                        output.print("\t\t\t\t\t\tnewrow.count_"+temp.attribute+"_"+temp.index+" = 0;\n");
                     }
                     else if (temp.aggregate.equals("min"))
-                        output.print("\t\t\t\t\tnewrow."+temp.getName()+" = Integer.MAX_VALUE;\n");
+                        output.print("\t\t\t\t\t\tnewrow."+temp.getName()+" = Integer.MAX_VALUE;\n");
                     else if (temp.aggregate.equals("max"))
-                        output.print("\t\t\t\t\tnewrow."+temp.getName()+" = 0;\n");
+                        output.print("\t\t\t\t\t\tnewrow."+temp.getName()+" = 0;\n");
                 }
             }
-            output.print("\t\t\t\t\tresult_list.add(newrow);\n");
+            output.print("\t\t\t\t\t\tresult_list.add(newrow);\n");
+            output.print("\t\t\t\t\t}\n");
             output.print("\t\t\t\t}\n");
             output.print("\t\t\t}\n");
             output.print("\t\t\tmore=rs.next();\n");
