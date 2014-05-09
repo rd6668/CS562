9405f86918ac88d32788c18e4077193138c29006
jgaoxxxx <gao.jian0630@gmail.com>
2014-05-09 15:32:37 -0400

    change project.java

--- CS562/src/project.java
+++ CS562/src/project.java
@@ -1,26 +1,30 @@
 import java.util.*;
+import java.io.*;
 
 public class project{
-	public static void main(String[] args){
+        public static void main(String[] args){
 
-		HashMap<String,String> db_struct = new HashMap<String, String>(); //connect to db and get info
-		Database.dbconnect();
-        	Database.dbinfo(db_struct);
+                HashMap<String,String> db_struct = new HashMap<String, String>(); //connect to db and get info
+                Database.dbconnect();
+                Database.dbinfo(db_struct);
 
-        	/*for (Map.Entry<String, String> entry : db_struct.entrySet()){
-        		//String key = entry.getKey();
-			//String value = entry.getValue();
-        		System.out.printf(entry.getValue()+"\t"+entry.getKey()+"\n");
-        	}*/
+                /*for (Map.Entry<String, String> entry : db_struct.entrySet()){
+                        //String key = entry.getKey();
+                        //String value = entry.getValue();
+                        System.out.printf(entry.getValue()+"\t"+entry.getKey()+"\n");
+                }*/
+
+                File file = new File("sampleQuery2.txt");
+                QueryStruct query_struct = new QueryStruct(); //read from query and get info
+                Query.query_process(query_struct, file);
+                //Query.output(query_struct);
+
+                Generator.generateCode(db_struct,query_struct);
+                //Generator2.generateCode(db_struct,query_struct);
 
 
-		QueryStruct query_struct = new QueryStruct(); //read from query and get info
-        	Query.query_process(query_struct);
-        	//Query.output(query_struct);
 
+                
 
-
-        	Generator.generateCode(db_struct,query_struct);
-
-	}
+        }
 }
